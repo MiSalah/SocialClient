61612394be284946edefffeae176c35dc646dae6
MiSalah <elbaidoury.salaheddine@gmail.com>
2021-06-06 23:06:10 +0100

    Display person's information, and its relationships

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('morgan');
 var bodyParser = require('body-parser');
-const neo4j = require('neo4j-driver')
+var neo4j = require('neo4j-driver')
 
 var app = express();
 
@@ -66,7 +66,7 @@ app.post('/person/add', function(req, res){
 
     var lastname = req.body.lastname;
     var firstname = req.body.firstname;
-    console.log(lastname);
+    //console.log(lastname);
     
     session.run("CREATE (a:Person {firstname: $firstnameP, lastname: $lastnameP}) RETURN a",
     { firstnameP: firstname, lastnameP: lastname})
@@ -105,8 +105,9 @@ app.post('/friends/connect', function(req, res){
     
     var name1 = req.body.name1;
     var name2 = req.body.name2;
+    var date = req.body.date;
 
-    session.run("MATCH (a:Person {lastname:$nameParam1}),(b:Person {lastname : $nameParam2}) MERGE(a)-[r:FRIENDS]->(b) RETURN a,b",{ nameParam1: name1 , nameParam2 :name2 })
+    session.run("MATCH (a:Person {lastname:$nameParam1}),(b:Person {lastname : $nameParam2}) MERGE(a)-[r:FRIENDS {since: $dateP}]->(b) RETURN a,b",{ nameParam1: name1 , nameParam2 :name2 , dateP: date})
     
         .then(function(result){
             res.redirect('/');
@@ -139,24 +140,45 @@ app.post('/person/born/add', function(req, res){
 
 });
 
-// Person detail route
+// add living place 
+app.post('/person/live/add', function(req, res){
+    
+    var firstname = req.body.firstname;
+    var lastname = req.body.lastname;
+    var city = req.body.city;
+    var country = req.body.country;
+    var since = req.body.since;
 
+    session.run("MATCH (a:Person {firstname:$firstnameP, lastname:$lastnameP}),(l:location {city: $cityP, country: $countryP}) MERGE(a)-[r:LIVE_IN {since: $sinceP}]->(l) RETURN a,l",
+    { firstnameP: firstname , lastnameP :lastname, cityP: city, countryP: country, sinceP: since })
+    
+        .then(function(result){
+            res.redirect('/');
+            //session.close();
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+
+});
+
+// Person detail route
 app.get('/person/:id', function(req, res){
 
     var id = req.params.id;
-    res.send(id);
 
-    session.run("MATCH(a:Person) WHERE id(a)=toInteger($idParam) RETURN a.lastname as lastname",
+    session.run("MATCH(a:Person) WHERE id(a)=toInteger($idParam) RETURN a.lastname as lastname , a.firstname as firstname",
     {idParam:id})
     .then(function(result){
 
         var lastname = result.records[0].get("lastname");
+        var firstname = result.records[0].get("firstname");
 
-        session.run("OPTIONAL MATCH (a:Person)-[r:BORN_IN]-(l:location) WHERE id(a)=toInteger($idParam) RETURN l.city as city, l.country. as country",
+        session.run("OPTIONAL MATCH (a:Person)-[r:BORN_IN]-(l:location) WHERE id(a)=toInteger($idParam) RETURN l.city as city, l.country as country",
         {idParam:id})
-        .then(function(result3){
-            var city = result3.records[0].get("city");
-            var country = result3.records[0].get("country");
+        .then(function(result2){
+            var city = result2.records[0].get("city");
+            var country = result2.records[0].get("country");
 
             session.run("OPTIONAL MATCH (a:Person)-[r:FRIENDS]-(b:Person) WHERE id(a)=toInteger($idParam) RETURN b",
             {idParam: id} )
@@ -164,30 +186,37 @@ app.get('/person/:id', function(req, res){
 
                 var friendsList = [];
 
-                result3.forEach(function(record){
-
+                result3.records.forEach(function(record){
                     if(record._fields[0] != null){
 
                         friendsList.push({
                             id : record._fields[0].identity.low,
-                            lastname: record._fields[0].properties.lastname,
-                            firstname: record._fields[0].properties.firstname
+                            lastname : record._fields[0].properties.lastname,
+                            firstname : record._fields[0].properties.firstname
                         });
                     }
                 });
-
-                res.render('person',{
-                    id:id,
+                
+                res.render('personsInfos',{
+                    id: id,
                     lastname: lastname,
                     firstname: firstname,
-                    city:city,
-                    country:country,
+                    city: city,
+                    country: country,
                     friends : friendsList
-                });
+                }); 
+
+                console.log(id);
+                console.log(firstname);
+                console.log(lastname);
+                console.log(city);
+                console.log(country);
+                console.log(friendsList);
 
             })
             .catch(function(error){
                 console.log(error);
+                return res.status(404).json({error})
             });
         });    
     });
